6fca4e5547d45a0bde86182cbe9753fdc7abbb77
Juliette Bourdiau <juliette.bourdiau@gmail.com>
2025-01-17 03:46:17 +0100

    user chat bubbles

--- Editor/UI/ChatWindow.cs
+++ Editor/UI/ChatWindow.cs
@@ -18,6 +18,8 @@ namespace ChatClass
         private string userInput = "";
         private Button submitButton;
 
+        private ScrollView chatContainer;
+
         public VisualElement CreateChatView()
         {
             chatVisualTree = AssetDatabase.LoadAssetAtPath<VisualTreeAsset>("Packages/com.jin.protochill/Editor/UI/ChatWindow.uxml");
@@ -29,6 +31,7 @@ namespace ChatClass
 
             inputField = chatCanvas.Q<TextField>("chat-input-field");
             submitButton = chatCanvas.Q<Button>("chat-submit-button");
+            chatContainer = chatCanvas.Q<ScrollView>("chat-messages-container");
 
             submitButton.clicked += OnSubmitButtonClick;
 
@@ -42,7 +45,19 @@ namespace ChatClass
             {
                 // Actuellement le seul abonné est l'instance de UMLDiag de main.
                 OnSubmitText?.Invoke("Make a UML for the system :" + userInput);
-                Debug.Log("Text submitted: " + userInput);
+
+                VisualElement bubble = new VisualElement();
+                bubble.AddToClassList("chat-user-bubble");
+                TextField content = new TextField();
+                content.AddToClassList("chat-user-text");
+                content.multiline = true;
+                content.value = userInput;
+                content.isReadOnly = true;
+                bubble.Add(content);
+
+                chatContainer.Add(bubble);
+                chatContainer.ScrollTo(bubble);
+
                 userInput = "";
                 inputField.value = "";
             }
